3be746ac645ef5e28a62d4583433bc40b60b20b5
Jibran023 <js08312@st.habib.edu.pk>
2024-06-03 00:34:37 +0500

    Playing with HTML
    Added links with the HTML

--- studyapp/base/urls.py
+++ studyapp/base/urls.py
@@ -6,6 +6,6 @@ from django.http import HttpResponse # to give the errors
  # All the URLS of our website will be here like Home, about etc.
 urlpatterns = [
     path('', views.home, name = "Home"), # This will be rendered when the user clicks the link. We have now set the direct path and set a name for it
-    path('room/', views.room, name = "Room"),
+    path('room/<str:pk>/', views.room, name = "Room"),
 ]
 
--- studyapp/base/views.py
+++ studyapp/base/views.py
@@ -3,13 +3,24 @@ from django.shortcuts import render
 
 # Create your views here. This will be what is rendered to the user when it goes to the specific url 
 
+rooms = [{'id': 1, 'name': 'Lets learn python!'},
+        {'id': 2, 'name': 'Lets learn C++!'},
+        {'id': 3, 'name': 'Lets learn Java!'},
+        {'id': 4, 'name': 'Lets learn Rust!'}]
 
 
 def home(request):
     # return HttpResponse('Wtf is wrong with you? This is the Home page') # this is the  home page and this will be rendered
-    return render(request, 'home.html') # this is the home page and this will be rendered now after we have added the templates
+    context = {'rooms' : rooms}
+    return render(request, 'base/home.html', context) # || this is the home page and this will be rendered now after we have added the templates
 
-def room(request):
+def room(request, pk):
     # return HttpResponse('This is the room page') # this is the room page. To access this page go to http://127.0.0.1:8000/room/
-    return render(request, 'room.html') # this is the home page and this will be rendered now after we have added the templates
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room' : room}
+
+    return render(request, 'base/room.html', context) # this is the home page and this will be rendered now after we have added the templates
 
